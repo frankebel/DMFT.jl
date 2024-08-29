6c0989969cd732c2a94c88bf0143a44635c22f4a
Frank Ebel <frank.ebel@student.tuwien.ac.at>
2024-08-29 18:23:48 +0200

    test: fix fencing of set

--- test/ED.jl
+++ test/ED.jl
@@ -13,13 +13,15 @@ using Fermions
     n = occupations(fs)
     H_int = U * n[1, 1//2] * n[1, -1//2]
 
-    @testset "solve_impurity_ed" begin end # solve_impurity_ed
-    G = solve_impurity_ed(Δ, H_int, ϵ_imp)
-    b = abs2.(G.b)
-    n_sites = n_bath + 1
-    @test length(G.a) == 2 * binomial(n_sites, n_sites ÷ 2) * binomial(n_sites, n_bath ÷ 2)
-    @test sum(b) ≈ 1 rtol = 10 * eps()
-    # PHS
-    @test norm(G.a + reverse(G.a)) < 150 * eps()
-    @test norm(b - reverse(b)) < 40 * eps()
+    @testset "solve_impurity_ed" begin
+        G = solve_impurity_ed(Δ, H_int, ϵ_imp)
+        b = abs2.(G.b)
+        n_sites = n_bath + 1
+        @test length(G.a) ==
+            2 * binomial(n_sites, n_sites ÷ 2) * binomial(n_sites, n_bath ÷ 2)
+        @test sum(b) ≈ 1 rtol = 10 * eps()
+        # PHS
+        @test norm(G.a + reverse(G.a)) < 150 * eps()
+        @test norm(b - reverse(b)) < 40 * eps()
+    end # solve_impurity_ed
 end # ED
