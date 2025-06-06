ebef3fc36df73b4dcd0107c412e724a4966ba8ce
Frank Ebel <frank.ebel@student.tuwien.ac.at>
2025-06-06 10:44:14 +0200

    refactor!: `grid_log` needs positive initial value

--- src/utility.jl
+++ src/utility.jl
@@ -224,9 +224,9 @@ function _get_filling(
 end
 
 """
-    grid_log(ω0::Real, Λ::Real, n::Int)
+    grid_log(ω_max::Real, Λ::Real, n::Int)
 
-Create `n` sorted poles on a logarithmic grid with `ω0` having highest magnitude.
+Create `n` sorted poles on a logarithmic grid with highest value `ω_max`.
 
 The poles have constant ratio
 
@@ -234,10 +234,11 @@ The poles have constant ratio
 \\frac{ω_i}{ω_{i+1}} =  \\frac{1}{Λ}.
 ```
 """
-function grid_log(ω0::Real, Λ::Real, n::Int)
+function grid_log(ω_max::Real, Λ::Real, n::Int)
+    ω_max >= 0 || throw(ArgumentError("negative frequency ω_max"))
     Λ > 1 || throw(ArgumentError("invalid discretization parameter Λ"))
     n >= 1 || throw(ArgumentError("invalid number of points n"))
-    result = map(i -> Λ^(-i) * ω0, 0.0:(n - 1))
-    ω0 >= 0 && reverse!(result) # from small to big values
+    result = map(i -> Λ^(-i) * ω_max, 0.0:(n - 1))
+    reverse!(result) # from small to big values
     return result
 end
--- test/utility.jl
+++ test/utility.jl
@@ -99,11 +99,11 @@ using Test
     @testset "logarithmic grid" begin
         @test_throws ArgumentError grid_log(1, 1.0, 10)
         @test_throws ArgumentError grid_log(1, 2.0, 0)
+        @test_throws ArgumentError grid_log(-16.0, 2.0, 1)
         @test grid_log(1, 2, 10) == [
             2^(-9), 2^(-8), 2^(-7), 2^(-6), 2^(-5), 2^(-4), 2^(-3), 2^(-2), 2^(-1), 2^(-0)
         ]
-        @test grid_log(16.0, 2, 4) == [2, 4, 8, 16]
-        @test grid_log(-16.0, 2, 4) == [-16, -8, -4, -2]
-        @test grid_log(100, 500, 1) == [100]
+        @test grid_log(16.0, 2, 4) == [2.0, 4.0, 8.0, 16.0]
+        @test grid_log(100, 500, 1) == [100.0]
     end # logarithmic grid
 end # util
