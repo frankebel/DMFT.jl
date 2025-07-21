94c94e60c7f6da9ff7b5bba25c58d946c955a431
Frank Ebel <frank.ebel@student.tuwien.ac.at>
2025-07-21 17:46:17 +0200

    feat: SVD orthonormalization (Löwdin) for Matrix of numbers.

--- src/orthogonalization.jl
+++ src/orthogonalization.jl
@@ -17,6 +17,21 @@ S^{-1/2} &= T Λ^{-1/2} T^†.
 
 Returns ``W = V S^{-1/2}`` and ``S^{1/2}``.
 """
+function _orthonormalize_SVD(V::AbstractMatrix{<:T}) where {T<:Number}
+    S = hermitianpart!(V' * V)
+    F = eigen!(S)
+    tol = sqrt(eps()) * maximum(F.values)
+    S_sqrt =
+        F.vectors * Diagonal(map(i -> i >= tol ? sqrt(i) : zero(i), F.values)) * F.vectors'
+    S_sqrt_inv =
+        F.vectors *
+        Diagonal(map(i -> i >= tol ? 1 / sqrt(i) : zero(i), F.values)) *
+        F.vectors'
+    hermitianpart!(S_sqrt)
+    hermitianpart!(S_sqrt_inv)
+    W = V * S_sqrt_inv
+    return W, S_sqrt
+end
 function _orthonormalize_SVD(V::AbstractMatrix{<:C}) where {C<:CIWavefunction}
     size(V, 1) == 1 || throw(ArgumentError("input matrix must have 1 row"))
     n = size(V, 2)
--- test/orthogonalization.jl
+++ test/orthogonalization.jl
@@ -5,6 +5,15 @@ using Test
 
 @testset "orthogonalization" begin
     @testset "_orthonormalize_SVD" begin
+        # Matrix{ComplexF64}
+        V = rand(ComplexF64, 10, 4)
+        @inferred DMFT._orthonormalize_SVD(V)
+        W, S_sqrt = DMFT._orthonormalize_SVD(V)
+        @test norm(W' * W - I) < 100 * eps() # W^† W = 𝟙
+        @test norm(V - W * S_sqrt) < 100 * eps() # V = W * S^{1/2}
+        @test ishermitian(S_sqrt)
+
+        # CIWavefunction
         v1 = CIWavefunction(Dict(zero(UInt8) => rand(5), one(UInt8) => rand(5)), 4, 1, 1, 1)
         v2 = CIWavefunction(Dict(zero(UInt8) => rand(5)), 4, 1, 1, 1)
         V = [v1 v2]
@@ -20,5 +29,6 @@ using Test
         for i in eachindex(V)
             @test norm(bar[i] - V[i]) < 8 * eps()
         end
+        @test issymmetric(S_sqrt)
     end # _orthonormalize_SVD
 end # orthogonalization
